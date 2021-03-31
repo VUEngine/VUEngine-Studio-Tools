1579544132bb573b9311fd067a51d1f182c66e01
KR155E <c.radke@posteo.de>
2021-03-31 11:44:30 +0200

    Change libretro.js for custom key bindings

--- web/retroarch/libretro.js
+++ web/retroarch/libretro.js
@@ -3,6 +3,7 @@ var afs;
 var romData;
 var coreOptions = "";
 var retroarchConfig = "";
+const canvas = document.getElementById("canvas");
 
 let dataURItoBuffer = (dataURI) => {
   let byteString = atob(dataURI.split(",")[1]);
@@ -20,7 +21,6 @@ async function processCommand(evt) {
   switch (evt.data.command) {
     case "start":
       romData = dataURItoBuffer(evt.data.data);
-      init();
       idbfsInit();
       break;
     case "setRetroArchConfig":
@@ -32,42 +32,10 @@ async function processCommand(evt) {
     case "clean":
       cleanupStorage();
       break;
-    case "togglePause":
-      keyPress("Space");
-      break;
-    case "reset":
-      keyPress("F10");
-      break;
-    case "fullscreen":
-      Module.requestFullscreen(true);
-      break;
-    case "mute":
-      keyPress("F3");
-      break;
-    case "increaseSaveSlot":
-      keyPress("F7");
-      break;
-    case "decreaseSaveSlot":
-      keyPress("F6");
-      break;
-    case "loadState":
-      Module._cmd_load_state();
-      break;
-    case "saveState":
-      Module._cmd_save_state();
-      break;
-    case "rewind":
-      keyPress("ArrowLeft");
-      break;
-    case "frameAdvance":
-      keyPress("ArrowUp");
-      break;
-    case "fastForward":
-      keyPress("ArrowRight");
+    case "keyPress":
+      keyPress(evt.data.data);
       break;
   }
-
-  document.getElementById("canvas").focus();
 }
 window.addEventListener("message", processCommand);
 
@@ -198,7 +166,6 @@ async function startRetroArch() {
   ]);
 
   Module.setCanvasSize(window.innerWidth, window.innerHeight);
-  document.getElementById("canvas").focus();
 }
 
 window.onresize = () => {
@@ -215,59 +182,18 @@ var Module = {
   printErr: function (text) {
     console.log(text);
   },
-  canvas: document.getElementById("canvas"),
+  canvas,
   totalDependencies: 0,
   monitorRunDependencies: function (left) {
     this.totalDependencies = Math.max(this.totalDependencies, left);
   },
 };
 
-function init() {
-  var keys = {
-    9: "tab",
-    13: "enter",
-    16: "shift",
-    18: "alt",
-    27: "esc",
-    32: "space",
-    33: "rePag",
-    34: "avPag",
-    35: "end",
-    36: "home",
-    37: "left",
-    38: "up",
-    39: "right",
-    40: "down",
-    112: "F1",
-    113: "F2",
-    114: "F3",
-    115: "F4",
-    116: "F5",
-    117: "F6",
-    118: "F7",
-    119: "F8",
-    120: "F9",
-    121: "F10",
-    122: "F11",
-    123: "F12",
-  };
-  window.addEventListener("keydown", function (e) {
-    if (keys[e.which]) {
-      e.preventDefault();
-    }
-  });
-}
-
 function keyPress(k) {
-  kp(k, "keydown");
+  canvas.focus();
+  document.dispatchEvent(new KeyboardEvent("keydown", { code: k }))
   setTimeout(function () {
-    kp(k, "keyup");
+    document.dispatchEvent(new KeyboardEvent("keyup", { code: k }))
   }, 50);
+  canvas.blur();
 }
-
-kp = function (k, event) {
-  var oEvent = new KeyboardEvent(event, { code: k });
-
-  document.dispatchEvent(oEvent);
-  document.getElementById("canvas").focus();
-};
